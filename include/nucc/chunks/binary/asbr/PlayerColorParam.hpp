2ebb5f19b0af1b11bf77efb73fa683feca52ddf6
Kojo Bailey <kojomolojo@gmail.com>
2024-09-16 19:10:09 +0300

    Integrate JSON

--- include/nucc/chunks/binary/asbr/PlayerColorParam.hpp
+++ include/nucc/chunks/binary/asbr/PlayerColorParam.hpp
@@ -10,7 +10,8 @@ class PlayerColorParam : public Binary_Data {
 public:
     struct Entry {
         std::uint64_t character_id_ptr;
-        std::string character_id;
+        std::string character_id; // Length always 6.
+        std::uint32_t tint_index; // Not written.
         std::uint32_t costume_index;
         std::uint32_t red, green, blue;
     };
@@ -21,48 +22,82 @@ public:
     std::vector<Entry> entries;
 
     PlayerColorParam(void* input, size_t size_input = -1) {
-        storage.load(input, 0, size_input);
-        version = storage.read<std::uint32_t>(kojo::endian::little);
-        entry_count = storage.read<std::uint32_t>(kojo::endian::little);
-        first_pointer = storage.read<std::uint64_t>(kojo::endian::little);
-        storage.change_pos(first_pointer - 8);
+        load(input, size_input);
+    }
+
+    void load(void* input, size_t size_input = -1) {
+        kojo::binary buffer{input, 0, size_input};
+
+        version = buffer.read<std::uint32_t>(kojo::endian::little);
+        entry_count = buffer.read<std::uint32_t>(kojo::endian::little);
+        first_pointer = buffer.read<std::uint64_t>(kojo::endian::little);
+        buffer.change_pos(first_pointer - 8);
         for (int i = 0; i < entry_count; i++) {
             entries.push_back({});
-            entries[i].character_id_ptr = storage.read<std::uint64_t>(kojo::endian::little);
-            entries[i].character_id = storage.read<std::string>(0, entries[i].character_id_ptr - 8);
-            entries[i].costume_index = storage.read<std::uint32_t>(kojo::endian::little);
-            entries[i].red = storage.read<std::uint32_t>(kojo::endian::little);
-            entries[i].green = storage.read<std::uint32_t>(kojo::endian::little);
-            entries[i].blue = storage.read<std::uint32_t>(kojo::endian::little);
+            auto& entry = entries[i];
+
+            entry.character_id_ptr  = buffer.read<std::uint64_t>(kojo::endian::little);
+            entry.character_id      = buffer.read<std::string>(0, entry.character_id_ptr - 8);
+            entry.costume_index     = buffer.read<std::uint32_t>(kojo::endian::little);
+            entry.red               = buffer.read<std::uint32_t>(kojo::endian::little);
+            entry.green             = buffer.read<std::uint32_t>(kojo::endian::little);
+            entry.blue              = buffer.read<std::uint32_t>(kojo::endian::little);
+
+            entry.tint_index = tint_tracker[std::format("{}-{}", entry.character_id, entry.costume_index)]++;
         }
+    }
+    void load(nlohmann::ordered_json input) {
+
     }
 
     size_t size() {
         return entry_count * 31 + 16;
     }
+    void clear() {
+        version = 0;
+        entry_count = 0;
+        first_pointer = 8;
+        entries.clear();
+        tint_tracker.clear();
+    }
 
-    kojo::binary& write() {
-        storage.clear();
+    kojo::binary write_to_bin() {
+        kojo::binary buffer;
         version = 1000;
         entry_count = entries.size();
-        storage.write<std::uint32_t>(version, kojo::endian::little);
-        storage.write<std::uint32_t>(entry_count, kojo::endian::little);
-        storage.write<std::uint64_t>(first_pointer, kojo::endian::little);
+        buffer.write<std::uint32_t>(version, kojo::endian::little);
+        buffer.write<std::uint32_t>(entry_count, kojo::endian::little);
+        buffer.write<std::uint64_t>(first_pointer, kojo::endian::little);
 
         size_t i{0};
         for (auto& entry : entries) {
-            entry.character_id_ptr = 24 * entry_count + i * 7 - i++ * 24;
-            storage.write<std::uint64_t>(entry.character_id_ptr, kojo::endian::little);
-            storage.write<std::uint32_t>(entry.costume_index, kojo::endian::little);
-            storage.write<std::uint32_t>(entry.red, kojo::endian::little);
-            storage.write<std::uint32_t>(entry.green, kojo::endian::little);
-            storage.write<std::uint32_t>(entry.blue, kojo::endian::little);
+            entry.character_id_ptr = (24 * entry_count) + (i * 7) - (i++ * 2);
+            buffer.write<std::uint64_t>(entry.character_id_ptr, kojo::endian::little);
+            buffer.write<std::uint32_t>(entry.costume_index, kojo::endian::little);
+            buffer.write<std::uint32_t>(entry.red, kojo::endian::little);
+            buffer.write<std::uint32_t>(entry.green, kojo::endian::little);
+            buffer.write<std::uint32_t>(entry.blue, kojo::endian::little);
         }
         for (auto& entry : entries) {
-            storage.write<std::string>(entry.character_id);
+            buffer.write<std::string>(entry.character_id);
         }
-        return storage;
+
+        return buffer;
     }
+    std::string write_to_json() {
+        nlohmann::ordered_json json;
+        std::string key_buffer;
+        
+        for (auto& entry : entries) {
+            key_buffer = std::format("{}0{}col{}", entry.character_id.substr(0, 4), entry.costume_index, entry.tint_index);
+            json[key_buffer] = std::format("#{:02x}{:02x}{:02x}", entry.red, entry.green, entry.blue);
+        }
+
+        return json.dump(2);
+    }
+
+private:
+    std::unordered_map<std::string, int> tint_tracker;
 };
 
     } // namespace ASBR
--- include/nucc/chunks/binary/binary_data.hpp
+++ include/nucc/chunks/binary/binary_data.hpp
@@ -2,19 +2,27 @@
 #define KOJO_NUCC_BINARY_DATA
 
 #include "../../external/binary-lib/binary/binary.hpp"
+#include <nlohmann/json.hpp>
 
-#include <vector>
-#include <cstdint>
+#include <format>
 
 namespace nucc {
 
 class Binary_Data {
 public:
-    virtual size_t size() { return 0; }
-    virtual kojo::binary& write() { return storage; }
+    // Load data into variables (different for each dervied class).
+    // Do not overwrite existing data, instead merging over.
+    virtual void load(void* input, size_t size_input = -1) {}
+    virtual void load(nlohmann::ordered_json input) {}
 
-protected:
-    kojo::binary storage;
+    // Return size of would-be binary data.
+    virtual size_t size() { return -1; }
+    // Clear all variables.
+    virtual void clear() {}
+
+    // Write to binary or JSON output.
+    virtual kojo::binary write_to_bin() { return 0; }
+    virtual nlohmann::ordered_json write_to_json() { return 0; }
 };
 
 } // namespace nucc
