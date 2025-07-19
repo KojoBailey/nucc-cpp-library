9cfb3d5ea1a1709a09ef2570ad651b4c955cb564
Kojo Bailey <kojomolojo@gmail.com>
2025-07-19 15:59:21 +0200

    Bug fixes

--- src/crc32.cpp
+++ src/crc32.cpp
@@ -16,22 +16,10 @@ void crc32::load(std::string str) {
         m_id = std::stoul(str, nullptr, 16);
     } else {
         m_id = hash(str);
-        kojo::binary::set_endian(id, std::endian::big);
+        kojo::binary::set_endian(m_id, std::endian::big);
     }
 }
 
 std::string crc32::string() {
     return std::format("{:08x}", m_id);
-}
-
-constexpr std::uint32_t crc32::hash(std::string_view str) {
-    std::uint32_t crc = 0xFFFFFFFF;
-
-    for (std::size_t i = 0; i < str.length(); ++i) {
-        std::uint8_t byte = static_cast<std::uint8_t>(str[i]);
-        std::uint8_t index = static_cast<std::uint8_t>((crc >> 24) ^ byte);
-        crc = (crc << 8) ^ crc32_hash_table[index];
-    }
-
-    return ~crc;
 }
