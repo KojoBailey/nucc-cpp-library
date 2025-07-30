e27bdd35537b8875f93da391ce60a5786e1a2b60
Kojo Bailey <kojomolojo@gmail.com>
2025-07-30 12:52:55 +0530

    Fix byteswap crc32 bug

--- src/crc32.cpp
+++ src/crc32.cpp
@@ -16,7 +16,7 @@ void crc32::load(std::string str) {
         m_id = std::stoul(str, nullptr, 16);
     } else {
         m_id = hash(str);
-        kojo::binary::set_endian(m_id, std::endian::big);
+        m_id = kojo::binary::set_endian(m_id, std::endian::big);
     }
 }
 
