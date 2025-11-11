3a33df91ee7fc34f5dffe958a0903df7b524f7a3
Kojo Bailey <kojomolojo@gmail.com>
2025-11-11 13:08:57 +0000

    xfbin::read

--- include/xfbin/xfbin.hpp
+++ include/xfbin/xfbin.hpp
@@ -14,11 +14,15 @@ public:
 	xfbin() = default;
 	~xfbin() = default;
 
+	std::string filename;
+
 	/* Error handling */
 
 	enum class error {
 		ok,
 		null_file,
+		file_signature,
+		version,
 	};
 
 	[[nodiscard]] inline static constexpr std::string_view error_string(error code)
@@ -36,6 +40,19 @@ public:
 	/* Loading */
 
 	[[nodiscard]] static auto load(const std::filesystem::path& path) -> std::expected<xfbin, error>;
+
+private:
+	static constexpr std::string_view MAGIC{"NUCC"};    // Required of an XFBIN to start with these 4 bytes.
+        static constexpr std::uint32_t VERSION{121};        // Expected of all relevant XFBINs.
+        static constexpr std::uint32_t HEADER_SIZE{12};
+
+	size_t m_size{0};
+
+	/* Reading */
+	auto read(kojo::binary_view) -> std::expected<void, error>;
+	auto read_header(kojo::binary_view&) -> std::expected<void, error>;
+	auto read_index(kojo::binary_view&) -> std::expected<void, error>;
+	auto read_chunks(kojo::binary_view&) -> std::expected<void, error>;
 };
 
 }
--- src/xfbin.cpp
+++ src/xfbin.cpp
@@ -1,11 +1,63 @@
 #include <xfbin/xfbin.hpp>
 
+using namespace kojo::binary_types;
+
 namespace kojo {
 
 auto xfbin::load(const std::filesystem::path& path)
 -> std::expected<xfbin, error>
 {
-	return std::unexpected{error::null_file};
+	xfbin result;
+        // cryptor.reset_state(); // Reset the cryptor state before loading a new file.
+
+        auto data_buffer = kojo::binary::load(path);
+        if (!data_buffer) {
+		// !TODO - elaborate on error cases
+                return std::unexpected{error::null_file};
+        }
+	kojo::binary data = *data_buffer;
+
+        result.m_size = data.size();
+        result.filename = path.stem().string();
+
+        auto error_check = result.read(data);
+	if (!error_check) {
+		return std::unexpected{error_check.error()};
+	}
+
+	return result;
+}
+
+auto xfbin::read(kojo::binary_view data)
+-> std::expected<void, error>
+{
+	return read_header(data)
+		.and_then([&](auto) { return read_index(data); })
+		.and_then([&](auto) { return read_chunks(data); });
+}
+
+auto xfbin::read_header(kojo::binary_view& data)
+-> std::expected<void, error>
+{
+	auto magic_input = data.read<str>(4);
+        if (magic_input != MAGIC) {
+                return std::unexpected{error::file_signature};
+        }
+
+        auto version_input = data.read<u32>(std::endian::big);
+        if (version_input != VERSION) {
+		return std::unexpected{error::version};
+        }
+
+        auto flags = data.read<u64>(std::endian::little); // !TODO Parse *all* of these at some point.
+
+        // Extract the second-lowest byte from flags
+        // uint8_t decryption_flag = (flags >> 8) & 0xFF;
+        // if (decryption_flag != 0) {
+        //         should_decrypt = true;
+        // }
+
+	return {};
 }
 
 }
